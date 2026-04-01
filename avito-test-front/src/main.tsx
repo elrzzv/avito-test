bb25a14f35e3072bbc72387c0924e66ada7fb822
elrzzv <89242464424e@gmail.com>
2026-04-01 13:06:42 +0300

    feat(deaps): add ant design ant design - ui library

--- avito-test-front/src/main.tsx
+++ avito-test-front/src/main.tsx
@@ -1,9 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router'
 import { HelmetProvider } from 'react-helmet-async'
+import './index.css'
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
