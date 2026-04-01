9a0cd7ad992443f5181edfa5f83c1153c96d12bf
elrzzv <89242464424e@gmail.com>
2026-04-01 13:15:45 +0300

    feat(ui): AdsPage layout made basic layout of AdsPage

--- avito-test-front/src/pages/ads/AdsPage.tsx
+++ avito-test-front/src/pages/ads/AdsPage.tsx
@@ -1,9 +1,37 @@
 import type { JSX } from "react";
+import { Helmet } from "react-helmet-async";
+import './AdsPage.css';
+import { Layout } from 'antd';
 
-export default function EditPage(): JSX.Element {
-  return(
+const { Header, Footer, Sider, Content } = Layout;
+
+export default function AdsPage(): JSX.Element {
+  return (
     <>
-      <div>it's an ads page</div>
+      <Helmet>
+        <title>Мои объявления - Avito</title>
+      </Helmet>
+      
+      <Layout className="layout-root">
+        <Header className="layout-header">
+          Header
+        </Header>
+        
+        <Layout>
+          <Sider width="25%" className="layout-sider">
+            Sider Bar
+          </Sider>
+          
+          <Layout>
+            <Content className="layout-content">
+              Content
+            </Content>
+            <Footer className="layout-footer">
+              Footer
+            </Footer>
+          </Layout>
+        </Layout>
+      </Layout>
     </>
-  )
+  );
 }
