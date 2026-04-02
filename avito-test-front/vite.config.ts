3190b4ef8e71a9dd7cdacdb616b8ec4e5352410e
elrzzv <89242464424e@gmail.com>
2026-04-02 14:04:49 +0300

    feat(api): add server types and API proxy shortcut

--- avito-test-front/vite.config.ts
+++ avito-test-front/vite.config.ts
@@ -3,5 +3,21 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      babel: {
+        plugins: [['babel-plugin-react-compiler', { target: '19' }]],
+      },
+    })
+  ],
+
+  //for url shortcut:
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080'
+      }
+    }
+  }
+
 })
