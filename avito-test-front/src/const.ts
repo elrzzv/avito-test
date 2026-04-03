0ad3bb727d9d6a8bce40d773e1d189cba0668854
elrzzv <89242464424e@gmail.com>
2026-04-03 18:15:02 +0300

    feat: move all constants to one file and write more constants

--- avito-test-front/src/const.ts
+++
@@ -1,6 +0,0 @@
-export const Paths = {
-  Ads: '/ads',
-  Product: '/ads/:id',
-  Edit: '/ads/:id/edit',
-  NotFound: '*'
-} as const
--- avito-test-front/src/types/constants.ts
+++ avito-test-front/src/types/constants.ts
@@ -3,3 +3,47 @@ export const ITEM_CATEGORIES = {
   REAL_ESTATE: 'real_estate',
   ELECTRONICS: 'electronics',
 } as const;
+
+export const PATHS = {
+  Ads: '/ads',
+  NotFound: '*'
+} as const
+
+export const PARAM_NAME = {
+  brand: 'Бренд',
+  model: 'Модель',
+  yearOfManufacture: 'Год производства',
+  transmission: 'Тип трансмиссии',
+  mileage: 'Пробег',
+  enginePower: 'Мощность двигателя',
+  type: 'Тип',
+  address: 'Адрес',
+  area: 'Площадь',  
+  floor: 'Этаж',
+  condition: 'Состояние',
+  color: 'Цвет'
+} as const
+
+export const TRANSLATIONS = {
+  // для electronics
+  phone: 'Телефон',
+  laptop: 'Ноутбук', 
+  misc: 'Разное',
+  new: 'Новый',
+  used: 'Б/У',
+  
+  // для auto
+  automatic: 'Автомат',
+  manual: 'Механика',
+  
+  // для real_estate
+  flat: 'Квартира',
+  house: 'Дом',
+  room: 'Комната'
+} as const;
+
+export const ALL_PARAMS_FIELDS = {
+  auto: ['brand', 'model', 'yearOfManufacture', 'transmission', 'mileage', 'enginePower'] as const,
+  real_estate: ['type', 'address', 'area', 'floor'] as const,
+  electronics: ['type', 'brand', 'model', 'condition', 'color'] as const
+} as const;
